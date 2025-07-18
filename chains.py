337f1846b185bfb6c488aae537b5b44b3144a9a5
Yogeshwaran B <yogiwar33@gmail.com>
2025-07-18 11:51:45 +0530

    added the model router(mocked)

--- chains.py
+++ chains.py
@@ -1,17 +1,27 @@
 import models
 import prompt
+from transformers import pipeline
 
+classifier = pipeline("zero-shot-classification", model="facebook/bart-large-mnli")
 
-def chat(query):
-    """
-    Simulates a chat response for the given query.
-    This function can be replaced with actual chatbot logic.
-    """
-    llm = models.get_llm()
-    prompt_template = prompt.get_prompt_template()
+candidate_intents = ["job_search", "resume_help", "roadmap_request", "greeting", "interview_tips", "general_question"]
 
-    chain= llm | prompt_template
-    # For now, we just return a simple response
+user_input = " I need a roadmap to learn backend development"
 
-    response = chain.invoke(query)
-    return response.content
+result = classifier(user_input, candidate_intents)
+
+print("Predicted Intent:", result["labels"][0])
+
+# def chat(query):
+#     """
+#     Simulates a chat response for the given query.
+#     This function can be replaced with actual chatbot logic.
+#     """
+#     llm = models.get_llm()
+#     prompt_template = prompt.get_prompt_template()
+
+#     chain= llm | prompt_template
+#     # For now, we just return a simple response
+
+#     response = chain.invoke(query)
+#     return response.content
